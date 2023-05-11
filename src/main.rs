7c9d80a719d0067afd2de2384b3d7e29291acdd9
Aashish Sharma <aashishsharma@Aashishs-MacBook-Pro.local>
2023-05-11 13:24:46 -0700

    setup .env

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{error::Error, io, process};
-
+use dotenv;
+use sqlx::Row;
 fn csv_parse() -> Vec<Set> {
     let mut rdr = csv::Reader::from_reader(io::stdin());
     let mut data: Vec<Set> = vec![];
@@ -22,7 +23,17 @@ fn csv_parse() -> Vec<Set> {
 
 struct Set {
     pub number: String,
+    //pub theme: String,
+    //pub sub_theme: String,
+    //pub year: String,
     pub set_name: String,
+    //pub minifigs: u32,
+    //pub pieces: u32,
+    //pub width: f32,
+    //pub height: f32,
+    //pub depth: f32,
+    //pub launch_date: date,
+    //pub exit_date: date,
 }
 
 
@@ -40,23 +51,14 @@ async fn create(set: &Set, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    //if let Err(err) = example() {
-    //    println!("error running example: {}", err);
-    //    process::exit(1);
-    //}
     // create connection pool
-    // let url = "";
-    // let pool = sqlx::postgres::PgPool::connect(url).await?;
+    let url = dotenv::var("DATABASE_URL").unwrap();
+    let pool = sqlx::postgres::PgPool::connect(&url).await.expect("something fucked");
         
     // create database migration
-    // sqlx::migrate!("./migrations").run(&pool).await?;
+    sqlx::migrate!("./migrations").run(&pool).await?;
 
-    // let set = Set {
-        // number: "10123-1".to_string(),
-        // set_name: "Cloud City".to_string(),
-    // };
-
-    // create(&set, &pool).await?;
-    csv_parse();
+    //create(&set, &pool).await?;
+    // csv_parse();
     Ok(())
 }
